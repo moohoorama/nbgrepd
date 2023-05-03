05c1c9b11fc98aa9ddf2180a6a36059d5265a991
yanoo.kim <yanoo.kim@kakaocorp.com>
2023-05-04 08:14:50 +0900

    fix testcase

--- daemon/env/env_test.go
+++ daemon/env/env_test.go
@@ -9,13 +9,14 @@ import (
 func TestEnv(t *testing.T) {
 	Convey("Env basic test", t, func(c C) {
 		conf := Get()
-		Println(conf.RediAuth)
+        So(conf.NumBytes, ShouldEqual, 524288)
+		Println(conf.NumBytes)
 
 		/* Change REDI_AUTH => "12345" */
-		os.Setenv("REDI_AUTH", "12345")
+		os.Setenv("NUMBYTES", "12345")
 		So(Reload(), ShouldBeNil)
 
 		conf = Get()
-		So(conf.RediAuth, ShouldEqual, "12345")
+		So(conf.NumBytes, ShouldEqual, 12345)
 	})
 }
